3bf1688b78e0a6497a2513ada80d641580c4cd1e
John Pham <johnphammail@gmail.com>
2022-12-24 16:53:28 -0800

    Add OG

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Snowfall from "react-snowfall";
 import { Card } from "../components/Card";
 
+const OG_IMAGE = "https://mello-christmas-card-2022.vercel.app/card.png";
+const DESCRIPTION = "Mello's Christmas Card 2022";
 export default function Home() {
   return (
     <>
@@ -10,6 +12,30 @@ export default function Home() {
         <meta name="description" content="Mello's Christmas Card 2022" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <title>Meta Tags — Preview, Edit and Generate</title>
+        <meta name="title" content={"Mello's Christmas Card 2022"}></meta>
+        <meta name="description" content={DESCRIPTION}></meta>
+
+        <meta property="og:type" content="website"></meta>
+        <meta
+          property="og:url"
+          content="https://mello-christmas-card-2022.vercel.app"
+        ></meta>
+        <meta property="og:title" content="Mello's Christmas Card 2022"></meta>
+        <meta property="og:description" content={DESCRIPTION}></meta>
+        <meta property="og:image" content={OG_IMAGE}></meta>
+
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta
+          property="twitter:url"
+          content="https://mello-christmas-card-2022.vercel.app"
+        ></meta>
+        <meta
+          property="twitter:title"
+          content="Mello's Christmas Card 2022"
+        ></meta>
+        <meta property="twitter:description" content={DESCRIPTION}></meta>
+        <meta property="twitter:image" content={OG_IMAGE}></meta>
       </Head>
       <main>
         <div>
